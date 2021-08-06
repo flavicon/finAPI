43361ceae52c47054156e962d235222c1b75a834
Flavio Silva <flavio.devjs@gmail.com>
2021-08-06 06:01:31 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+
+  const customerAlreadyExists = customers.some(
+    customer => customer.cpf === cpf
+  )
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists"})
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: []
+  });
+
+  return response.status(201).send()
+});
+
+app.get("/statement", (request, response) => {
+  const { cpf } = request.headers;
+
+  const customer = customers.find(customer => customer.cpf === cpf)
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found" })
+  }
+
+  return response.json(customer.statement)
+})
+
+app.listen(3333)
